717855a2084ace7f2335833a20f0bed255c7f50a
o0xmuhe <Conc0rd@163.com>
2018-02-01 14:06:28 +0800

    fix bugs

--- muhe_bot.py
+++ muhe_bot.py
@@ -6,7 +6,7 @@
 # Created Time: 2018-02-01 10:08:22
 
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-import SearchCVE
+from SearchCVE import *
 import logging
 import json
 import os,re
